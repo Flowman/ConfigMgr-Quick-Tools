01d060220a09de3eeec464305f385ef6246a50a6
Peter Szalatnay <theotherland@gmail.com>
2018-09-19 16:49:17 +0800

    placeholder for driver grabber

---
+++ DriverGrabberDialog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ConfigurationManagement.AdminConsole;
+using Microsoft.ConfigurationManagement.ManagementProvider;
+using Microsoft.ConfigurationManagement.AdminConsole.Schema;
+using System.DirectoryServices;
+using System.Windows.Forms;
+using System;
+
+namespace Zetta.ConfigMgr.QuickTools
+{
+    public partial class DriverGrabberDialog : SmsCustomDialog
+    {
+        private IResultObject resultObject;
+
+        public DriverGrabberDialog(IResultObject selectedResultObject)
+        {
+            resultObject = selectedResultObject;
+            InitializeComponent();
+        }
+
+        public static void Show(object sender, ScopeNode scopeNode, ActionDescription action, IResultObject selectedResultObject, PropertyDataUpdated dataUpdatedDelegate, Status status)
+        {
+            new DriverGrabberDialog(selectedResultObject).Show();
+        }
+    }
+}
